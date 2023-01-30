4bd945f6127823633023bbcbe4693035239bffcc
alpin87 <qortmdals94@naver.com>
2023-01-30 15:21:53 +0900

    code_test

---
+++ Main1269.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main1269 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        TreeSet<Integer> ts = new TreeSet<Integer>();
+        TreeSet<Integer> as = new TreeSet<Integer>();
+        for (int i=0; i<a; i++){
+            ts.add(sc.nextInt());
+        }
+        for (int j=0; j<b; j++){
+            as.add(sc.nextInt());
+        }
+
+
+
+
+    }
+}
