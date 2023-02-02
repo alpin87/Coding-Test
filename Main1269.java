39c761fab0beaaea784b8fef0e617242e5153e05
alpin87 <qortmdals94@naver.com>
2023-02-02 11:49:55 +0900

    230202

--- Main1269.java
+++ Main1269.java
@@ -1,24 +1,40 @@
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main1269 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        TreeSet<Integer> ts = new TreeSet<Integer>();
-        TreeSet<Integer> as = new TreeSet<Integer>();
-        for (int i=0; i<a; i++){
-            ts.add(sc.nextInt());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < a; i++) {
+            setA.add(Integer.parseInt(st.nextToken()));
         }
-        for (int j=0; j<b; j++){
-            as.add(sc.nextInt());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < b; i++) {
+            setB.add(Integer.parseInt(st.nextToken()));
         }
 
-
-
-
+        int ans = 0;
+        for(int num : setA) {
+            if(!setB.contains(num)) {
+                ans += 1;
+            }
+        }
+        for(int num : setB) {
+            if(!setA.contains(num)) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
     }
 }
