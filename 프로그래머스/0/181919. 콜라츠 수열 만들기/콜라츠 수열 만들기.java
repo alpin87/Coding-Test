33bcb24f4a2a792e212b271aea993103147c9a7b
baek seung min <35371121+alpin87@users.noreply.github.com>
2024-01-24 18:16:29 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        List<Integer> answerList = new ArrayList<>();
+        answerList.add(n);
+        
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            answerList.add(n);
+        }    
+
+        int[] answer = new int[answerList.size()];
+        
+        for(int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
