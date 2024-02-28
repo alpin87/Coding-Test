33be388f8df406fc7d8cd584e94a510ce37db3de
baek seung min <35371121+alpin87@users.noreply.github.com>
2024-02-28 17:30:08 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] students = new int[n+2];
+
+        for (int l : lost) {
+            students[l]--;
+        }
+
+        for (int r : reserve) {
+            students[r]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (students[i] == -1) {
+                if (students[i-1] == 1) {
+                    students[i-1]--;
+                    students[i]++;
+                } else if (students[i+1] == 1) {
+                    students[i+1]--;
+                    students[i]++;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (students[i] >= 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
