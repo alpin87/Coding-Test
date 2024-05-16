a70c244ccf6348ebb384166c628d07b19151203a
baek seung min <35371121+alpin87@users.noreply.github.com>
2024-05-16 17:42:39 +0900

    [level 0] Title: 접미사 배열, Time: 0.19 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] suffixArray = new String[my_string.length()];
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            suffixArray[i] = my_string.substring(i);
+        }
+
+        Arrays.sort(suffixArray);
+        
+        return suffixArray;
+    }
+}
