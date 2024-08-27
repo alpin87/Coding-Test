0dd3be947d347bdf70a7d912ed2b1a0892d8e63b
baek seung min <35371121+alpin87@users.noreply.github.com>
2024-08-27 18:28:03 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,6 +1,5 @@
 public class Solution {
     public static int[] solution(int[] num_list, int n) {
-        // num_list의 길이를 기반으로 새로운 배열 생성
         int[] result = new int[num_list.length];
 
         System.arraycopy(num_list, n, result, 0, num_list.length - n);
