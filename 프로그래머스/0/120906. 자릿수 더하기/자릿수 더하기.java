791ac6b0f2c879ab31e5da7ee0037c571cc12bbf
baek seung min <35371121+alpin87@users.noreply.github.com>
2024-12-01 19:53:25 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String strNum = Integer.toString(n);
+        
+        for(int i = 0; i < strNum.length(); i++) {
+            answer += Character.getNumericValue(strNum.charAt(i));
+        }
+        
+        return answer;
+    }
+}
